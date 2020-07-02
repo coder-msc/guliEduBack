bb6b3f230186e89074f720b8ebe17e2a1e7be832
jkma <1771947201@qq.com>
2020-07-03 00:33:47 +0800

    添加路由

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduLoginController.java
@@ -0,0 +1,23 @@
+package com.atguigu.eduservice.controller;
+
+import com.atguigu.commonutils.R;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/eduservice/user")
+@CrossOrigin
+public class EduLoginController {
+
+
+    @PostMapping("login")
+    public R Login(){
+
+        return  R.ok().data("token","admin");
+    }
+
+    @GetMapping("info")
+    public R GetInfo(){
+        return  R.ok().data("roles","[admin]").data("name","admin").data("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+    }
+}
