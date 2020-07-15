8cc80fea5f39de458f1a53c44ad922336b0164f7
jkma <1771947201@qq.com>
2020-07-16 00:28:59 +0800

    【代码添加】课程小节信息添加

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
@@ -1,10 +1,11 @@
 package com.atguigu.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduVideo;
+import com.atguigu.eduservice.service.EduVideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -15,9 +16,36 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-07-11
  */
 @RestController
-@RequestMapping("/eduservice/edu-video")
+@RequestMapping("/eduservice/video")
 @CrossOrigin
 public class EduVideoController {
 
+    @Autowired
+    private EduVideoService videoService;
+
+    //添加小节
+    @PostMapping("addVideo")
+    public R addvideo(@RequestBody EduVideo eduVideo){
+        videoService.save(eduVideo);
+        return R.ok();
+    }
+    //查小节
+    @GetMapping("getVideoById/{videoId}")
+    public R getVideoById(@PathVariable String videoId){
+        EduVideo eduvideo = videoService.getById(videoId);
+        return R.ok().data("eduvideo",eduvideo);
+    }
+    //修改小节
+    @PostMapping("updateVideo")
+    public R updateVideo(@RequestBody EduVideo eduVideo){
+        videoService.updateById(eduVideo);
+        return  R.ok();
+    }
+    //删除小节
+    @DeleteMapping("{videoId}")
+    public R deledtVideo(@PathVariable String videoId){
+        videoService.removeById(videoId);
+        return R.ok();
+    }
 }
 
