b89949191fce2395381c80750a1000712d700a4b
jkma <1771947201@qq.com>
2020-08-01 00:28:46 +0800

    【代码添加】登录注册 后台接口完成

---
+++ service/service_ucenter/src/main/java/com/atguigu/educenter/controller/UcenterMemberController.java
@@ -0,0 +1,41 @@
+package com.atguigu.educenter.controller;
+
+
+import com.atguigu.commonutils.R;
+import com.atguigu.educenter.entity.UcenterMember;
+import com.atguigu.educenter.entity.vo.RegisterVo;
+import com.atguigu.educenter.service.UcenterMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ *
+ * @author scma
+ * @since 2020-08-01
+ */
+@RestController
+@RequestMapping("/educenter/member")
+@CrossOrigin
+public class UcenterMemberController {
+
+    @Autowired
+    private UcenterMemberService userService;
+
+    //登录
+    @PostMapping("login")
+    public R loginUser(@RequestBody UcenterMember member) {
+        //member对象封装手机号和密码
+        //调用service方法实现登录
+        //返回token值，使用jwt生成
+        String token = userService.login(member);
+        return R.ok().data("token",token);
+    }
+
+    //注册
+    @PostMapping("register")
+    public R registerUser(@RequestBody RegisterVo registerVo) {
+        userService.register(registerVo);
+        return R.ok();
+    }
+}
+
---
+++ service/service_ucenter/src/main/java/com/atguigu/educenter/service/UcenterMemberService.java
@@ -0,0 +1,21 @@
+package com.atguigu.educenter.service;
+
+import com.atguigu.educenter.entity.UcenterMember;
+import com.atguigu.educenter.entity.vo.RegisterVo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 会员表 服务类
+ * </p>
+ *
+ * @author scma
+ * @since 2020-08-01
+ */
+public interface UcenterMemberService extends IService<UcenterMember> {
+    //登录的方法
+    String login(UcenterMember member);
+
+    //注册的方法
+    void register(RegisterVo registerVo);
+}
