9256ce24cd37884db004e7cc3e1d5caf704316ac
jkma <1771947201@qq.com>
2020-08-03 00:50:33 +0800

    【代码添加】微信扫码登录-获取到用户信息

---
+++ service/service_ucenter/src/main/java/com/atguigu/educenter/controller/WxApiController.java
@@ -0,0 +1,112 @@
+package com.atguigu.educenter.controller;
+
+import com.atguigu.educenter.service.UcenterMemberService;
+import com.atguigu.educenter.utils.ConstantWxUtils;
+import com.atguigu.educenter.utils.HttpClientUtils;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URLEncoder;
+import java.util.HashMap;
+
+@Controller
+@CrossOrigin
+@RequestMapping("/api/ucenter/wx")
+public class WxApiController {
+
+    @Autowired
+    private UcenterMemberService memberService;
+
+    //2扫描之后获取信息
+    @GetMapping("callback")
+    public String getInfo(String code,String state){
+        try {
+            //1 获取code值，临时票据，类似于验证码
+            //2 拿着code请求 微信固定的地址，得到两个值 accsess_token 和 openid
+            String baseAccessTokenUrl = "https://api.weixin.qq.com/sns/oauth2/access_token" +
+                    "?appid=%s" +
+                    "&secret=%s" +
+                    "&code=%s" +
+                    "&grant_type=authorization_code";
+            //拼接三个参数 ：id  秘钥 和 code值
+            String accessTokenUrl = String.format(
+                    baseAccessTokenUrl,
+                    ConstantWxUtils.WX_OPEN_APP_ID,
+                    ConstantWxUtils.WX_OPEN_APP_SECRET,
+                    code
+            );
+            //请求这个拼接好的地址，得到返回两个值 accsess_token 和 openid
+            //使用httpclient发送请求，得到返回结果
+            String accessTokenInfo = HttpClientUtils.get(accessTokenUrl);
+
+
+            Gson gson = new Gson();
+            HashMap mapAccess = gson.fromJson(accessTokenInfo, HashMap.class);
+            String access_token =(String) mapAccess.get("access_token");
+            String openid =(String) mapAccess.get("openid");
+// 用以上两个值 再去获取到扫码人的基本信息
+            //访问微信的资源服务器，获取用户信息
+            String baseUserInfoUrl = "https://api.weixin.qq.com/sns/userinfo" +
+                    "?access_token=%s" +
+                    "&openid=%s";
+            //拼接两个参数
+            String userInfoUrl = String.format(
+                    baseUserInfoUrl,
+                    access_token,
+                    openid
+            );
+            //发送请求
+            String userInfo = HttpClientUtils.get(userInfoUrl);
+            //获取返回userinfo字符串扫描人信息
+            HashMap userInfoMap = gson.fromJson(userInfo, HashMap.class);
+            String nickname = (String)userInfoMap.get("nickname");//昵称
+            String headimgurl = (String)userInfoMap.get("headimgurl");//头像
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+            return "redirect:http://localhost:3000";
+    }
+
+    //1 生成微信扫描二维码
+    @GetMapping("login")
+    public String getWxCode() {
+        //固定地址，后面拼接参数
+//        String url = "https://open.weixin.qq.com/" +
+//                "connect/qrconnect?appid="+ ConstantWxUtils.WX_OPEN_APP_ID+"&response_type=code";
+
+        // 微信开放平台授权baseUrl  %s相当于?代表占位符
+        String baseUrl = "https://open.weixin.qq.com/connect/qrconnect" +
+                "?appid=%s" +
+                "&redirect_uri=%s" +
+                "&response_type=code" +
+                "&scope=snsapi_login" +
+                "&state=%s" +
+                "#wechat_redirect";
+
+        //对redirect_url进行URLEncoder编码
+        String redirectUrl = ConstantWxUtils.WX_OPEN_REDIRECT_URL;
+        try {
+            redirectUrl = URLEncoder.encode(redirectUrl, "utf-8");
+        }catch(Exception e) {
+        }
+
+        //设置%s里面值
+        String url = String.format(
+                baseUrl,
+                ConstantWxUtils.WX_OPEN_APP_ID,
+                redirectUrl,
+                "atguigu"
+        );
+
+        //重定向到请求微信地址里面
+        return "redirect:"+url;
+    }
+}
