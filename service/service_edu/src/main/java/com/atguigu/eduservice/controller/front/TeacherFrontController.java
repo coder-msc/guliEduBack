236b61a36b44d36212fc0579a7dc34280df793d5
jkma <1771947201@qq.com>
2020-08-05 00:16:12 +0800

    【代码添加】课程列表带分页的

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/TeacherFrontController.java
@@ -0,0 +1,36 @@
+package com.atguigu.eduservice.controller.front;
+
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduCourseService;
+import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/eduservice/teacherfront")
+@CrossOrigin
+public class TeacherFrontController {
+    @Autowired
+    private EduTeacherService teacherService;
+
+    // 讲师列表 分页  当前页  每页记录数
+    @Autowired
+    private EduTeacherService teacherService1;
+
+    @Autowired
+    private EduCourseService courseService;
+
+    //1 分页查询讲师的方法
+    @PostMapping("getTeacherFrontList/{page}/{limit}")
+    public R getTeacherFrontList(@PathVariable long page,@PathVariable long limit) {
+        Page<EduTeacher> pageTeacher = new Page<>(page,limit);
+        Map<String,Object> map = teacherService1.getTeacherFront(pageTeacher);
+        //返回分页所有数据
+        return R.ok().data(map);
+    }
+}
--- service/service_edu/src/main/java/com/atguigu/eduservice/service/EduTeacherService.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/EduTeacherService.java
@@ -1,7 +1,10 @@
 package com.atguigu.eduservice.service;
 
 import com.atguigu.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -12,5 +15,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-06-20
  */
 public interface EduTeacherService extends IService<EduTeacher> {
-
+    // 讲师列表 分页  当前页  每页记录数
+    Map<String, Object> getTeacherFront(Page<EduTeacher> page1);
 }
