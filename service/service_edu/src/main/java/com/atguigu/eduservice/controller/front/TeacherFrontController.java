72d1985992b40b287f59c4c929b13c5050d1eed0
jkma <1771947201@qq.com>
2020-08-05 00:45:38 +0800

    【代码添加】教师详情，所教课程

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/TeacherFrontController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/TeacherFrontController.java
@@ -1,14 +1,17 @@
 package com.atguigu.eduservice.controller.front;
 
 import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduCourse;
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduCourseService;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -33,4 +36,17 @@ public class TeacherFrontController {
         //返回分页所有数据
         return R.ok().data(map);
     }
+
+    @GetMapping("getTeacherFrontInfo/{teacherId}")
+    public R geteacherInfo(@PathVariable String teacherId){
+        //教师基本信息
+        EduTeacher eduTeacher = teacherService1.getById(teacherId);
+//教师所讲课程
+        QueryWrapper<EduCourse> wrapper = new QueryWrapper<>();
+        wrapper.eq("teacher_id",teacherId);
+        List<EduCourse> courseList = courseService.list(wrapper);
+
+        return R.ok().data("teacher",eduTeacher).data("courseList",courseList);
+    }
+
 }
