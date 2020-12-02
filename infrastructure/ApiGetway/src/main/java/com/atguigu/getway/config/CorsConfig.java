8cc44f6c896451723557a473f27739348b3a394d
scma <59968873@qq.com>
2020-12-02 23:54:24 +0800

    【新增修改】引入gateway网关，做服务代理

---
+++ infrastructure/ApiGetway/src/main/java/com/atguigu/getway/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.atguigu.getway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+/**
+ * <p>
+ *gateway 处理跨域问题
+ * </p>
+ **/
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
+
