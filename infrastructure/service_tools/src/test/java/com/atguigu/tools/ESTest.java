32526b581e1e0a6fb33aa6a9c94ab83dad1da9d8
scma <59968873@qq.com>
2021-02-03 22:59:17 +0800

    ES的CRUD测试

--- infrastructure/service_tools/src/test/java/com/atguigu/tools/ESTest.java
+++ infrastructure/service_tools/src/test/java/com/atguigu/tools/ESTest.java
@@ -8,13 +8,22 @@ import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,6 +32,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -64,7 +74,7 @@ public class ESTest {
         UcenterMember ucenterMember= new UcenterMember();
         ucenterMember.setAvatar("hello");
         ucenterMember.setAge(32);
-        ucenterMember.setNickname("小明小明");
+        ucenterMember.setNickname("xiaoming");
         //创建请求
         IndexRequest request = new IndexRequest("kuang_index");
         //规则 put  /kuang_index/_doc/1
@@ -93,4 +103,55 @@ public class ESTest {
     }
 
 
+                /*设置这两条记录进去
+
+
+                PUT testdb
+                {
+                  "mappings": {
+                    "properties": {
+                      "name":{
+                        "type": "text"
+                      },
+                      "desc":{
+                        "type": "keyword"
+                      }
+                    }
+                  }
+                }
+
+            PUT testdb/_doc/1
+            {
+              "name":"123 java 好",
+              "desc":"123331ava desc"
+            }
+
+            PUT testdb/_doc/2
+            {
+              "name":"好",
+              "desc":"234242Java desc2"
+            }
+                * */
+    //查询
+    @Test
+    public void Testsearch() throws IOException {
+        SearchRequest searchRequest=  new SearchRequest("testdb");
+        //构建搜索条件
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //查询条件
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("name", "好");
+//        QueryBuilders.matchAllQuery(); // 匹配所有查询
+        SearchSourceBuilder query = searchSourceBuilder.query(termQueryBuilder);
+        searchSourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));//查询超时时间
+
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse search = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHits searchHits = search.getHits();
+        System.out.println(JSON.toJSONString(searchHits.getHits()));
+        System.out.println("=============");
+        for(SearchHit document: searchHits.getHits()){
+            System.out.println(document.getSourceAsMap());
+        }
+
+    }
 }
