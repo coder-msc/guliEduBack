34a1a6f53fb004263d0d67fe7b6336859a183092
scma <59968873@qq.com>
2021-02-09 23:02:43 +0800

    ES简单测试使用

--- infrastructure/service_tools/src/main/java/com/atguigu/tools/controller/ESContentController.java
+++ infrastructure/service_tools/src/main/java/com/atguigu/tools/controller/ESContentController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class ESContentController {
     @Autowired
@@ -14,5 +17,14 @@ public class ESContentController {
     @GetMapping("/parse/{keyword}")
     public Boolean parse(@PathVariable("keyword") String keyword)throws  Exception{
         return elSearchService.parseContent(keyword);
+    }
+
+    //查询基本的操作  带分页
+    @GetMapping("/search/{pageNo}/{pageSize}/{keyword}")
+    public List<Map<String,Object>> searchPage(@PathVariable("keyword") String keyword,
+                                               @PathVariable("pageNo")  int pageNo,
+                                               @PathVariable("pageSize")  int pageSize) throws Exception {
+        List<Map<String, Object>> mapList = elSearchService.searchPage(keyword, pageNo, pageSize);
+        return mapList;
     }
 }
